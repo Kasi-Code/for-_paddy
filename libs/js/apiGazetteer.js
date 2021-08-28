3d26c7077c542af7f72ccfefba10e4b3c2bac351
Kasi-Code <64224482+Kasi-Code@users.noreply.github.com>
2021-08-28 12:58:54 +0100

    time fixed

--- libs/js/apiGazetteer.js
+++ libs/js/apiGazetteer.js
@@ -154,11 +154,14 @@ window.onload = function () {
                             $("#changeIcon").html("☁️")
                         } else if (weatherDescription.includes("rain") || weatherDescription.includes("rainny") || weatherDescription.includes("overcast") || weatherDescription.includes("drizzle")){
                             $("#changeIcon").html("&#127783")
-                        } else if (weatherDescription.includes("cloud") || weatherDescription.includes("clouds")){   
-
-                            (getTime >= 6 && getTime <= 18) ? $("#changeIcon").html("&#127780") : $("#changeIcon").html(cloudyNight)
-
-                            $(".cloudyPicture").css("display", "block")
+                        } else if (weatherDescription.includes("cloud") || weatherDescription.includes("clouds")){  
+                            
+                            if (getHour >= 6 && getHour <= 18) {
+                                $("#changeIcon").html("&#127780")
+                            } else {
+                                $("#changeIcon").html(cloudyNight)
+                                $(".cloudyPicture").css("display", "block")
+                            }                           
 
                         } else if (weatherDescription.includes("snow")){
                             $("#changeIcon").html("❄️")
