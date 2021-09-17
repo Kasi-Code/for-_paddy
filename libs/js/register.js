a9f50c6b719c673d2f5a07b7cf5c300f3abfb114
Kasi-Code <64224482+Kasi-Code@users.noreply.github.com>
2021-09-17 08:48:20 +0100

    still not working

--- libs/js/register.js
+++ libs/js/register.js
@@ -1,11 +1,11 @@
 const register = () => {
 
     event.preventDefault();
-    
+
     var xmlhttp;
 
     xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("GET", "register.php?username=" + document.getElementById("username").value + "&password=" + document.getElementById("password").value, false)
+    xmlhttp.open("GET", "lips/php/register.php?username=" + document.getElementById("username").value + "&password=" + document.getElementById("password").value, false)
     xmlhttp.send(null);
 
 }
