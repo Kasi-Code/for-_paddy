bd6202e41034030cad0bec9928879ed9e4751cb5
Kasi-Code <64224482+Kasi-Code@users.noreply.github.com>
2021-09-17 18:59:20 +0100

    php files loaded

--- libs/js/cDirectory.js
+++ libs/js/cDirectory.js
@@ -24,9 +24,10 @@ const formSubmit = ()=> {
 const readFormData = ()=> {
     let formData = {};
     
+    formData["id_number"] = document.getElementById("id_number").value;
     formData["first_name"] = document.getElementById("first_name").value;
     formData["last_name"] = document.getElementById("last_name").value;
-    formData["position"] = document.getElementById("position").value;
+    formData["department"] = document.getElementById("department").value;
     formData["location"] = document.getElementById("location").value;
     return formData;
 }
@@ -36,15 +37,17 @@ const insertNewRecord = (data)=> {
     var table = document.getElementById("list").getElementsByTagName('tbody')[0];
     var newRow = table.insertRow(table.length);
     cell1 = newRow.insertCell(0);
-    cell1.innerHTML = data.first_name;
+    cell1.innerHTML = data.id_number;
     cell2 = newRow.insertCell(1);
-    cell2.innerHTML = data.last_name;
+    cell2.innerHTML = data.first_name;
     cell3 = newRow.insertCell(2);
-    cell3.innerHTML = data.position;
+    cell3.innerHTML = data.last_name;
     cell4 = newRow.insertCell(3);
-    cell4.innerHTML = data.location;
+    cell4.innerHTML = data.department;
     cell5 = newRow.insertCell(4);
-    cell5.innerHTML = `<button class="editDeleteBtm editBtn" onClick="onEdit(this)">Edit</button>
+    cell5.innerHTML = data.location;
+    cell6 = newRow.insertCell(5);
+    cell6.innerHTML = `<button class="editDeleteBtm editBtn" onClick="onEdit(this)">Edit</button>
                        <button class="editDeleteBtm deleteBtn" onClick="onDelete(this)">Delete</button>`;
 
 
@@ -53,27 +56,31 @@ const insertNewRecord = (data)=> {
     cell3.setAttribute("class", "tableBody");
     cell4.setAttribute("class", "tableBody");
     cell5.setAttribute("class", "tableBody");
+    cell6.setAttribute("class", "tableBody");
 }
 
 const resetForm = ()=> {
+    document.getElementById("id_number").value = "";
     document.getElementById("first_name").value = "";
     document.getElementById("last_name").value = "";
-    document.getElementById("position").value = "";
+    document.getElementById("department").value = "";
     document.getElementById("location").value = "";
     selectedRow = null;
 }
 
 const onEdit = (td)=> {
     selectedRow = td.parentElement.parentElement;
-    document.getElementById("first_name").value = selectedRow.cells[0].innerHTML;
-    document.getElementById("last_name").value = selectedRow.cells[1].innerHTML;
-    document.getElementById("position").value = selectedRow.cells[2].innerHTML;
-    document.getElementById("location").value = selectedRow.cells[3].innerHTML;
+    document.getElementById("id_number").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("first_name").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("last_name").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("department").value = selectedRow.cells[3].innerHTML;
+    document.getElementById("location").value = selectedRow.cells[4].innerHTML;
 }
 const updateRecord = (formData)=> {
+    selectedRow.cells[0].innerHTML = formData.id_number;
     selectedRow.cells[0].innerHTML = formData.first_name;
     selectedRow.cells[1].innerHTML = formData.last_name;
-    selectedRow.cells[2].innerHTML = formData.position;
+    selectedRow.cells[2].innerHTML = formData.department;
     selectedRow.cells[3].innerHTML = formData.location;
 }
 
@@ -96,3 +103,19 @@ const onDelete = (td)=> {
 //     }
 //     return isValid;
 // }
+
+$(window).on("load", () => {
+    $.ajax({
+        url: "companydirectory/libs/php/getAllDepartments.php",
+        type: "GET",
+        dataType: "json",
+        success: function(result) {
+            result["data"].forEach(department => {
+
+                console.log(department)
+                // $("#deptSearch").append(`<option value="${department["id"]}">${department["name"]}</option>`);
+                // $(".deptEdit").append(`<option value="${department["id"]}">${department["name"]}</option>`);
+            })
+        }
+    })
+})
--- libs/php/register.php
+++ libs/php/register.php
@@ -1,25 +1,54 @@
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
 
+// // Create connection
+// $conn = new mysqli($servername, $username, $password);
+// // Check connection
+// if ($conn->connect_error) {
+//   die("Connection failed: " . $conn->connect_error);
+// }
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+// // Create database
+// $sql = "CREATE DATABASE register";
+// if ($conn->query($sql) === TRUE) {
+//   echo "Database created successfully";
+// } else {
+//   echo "Error creating database: " . $conn->error;
+// }
 
-  <?php
+// $conn->close();
 
-  $username = $_POST_POST["username"];
-  $password = $_POST_POST["password"];
+// database connection code
+// $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
+
+// $con = mysqli_connect($servername, $username, $password);
+
+// // get the post records
+// $username = $_POST['username'];
+// $password = $_POST['password'];
+
+// // database insert SQL code
+// $sql = "INSERT INTO `registerTest` (`username`, `password`) 
+// VALUES ('0', '$username', '$password')";
+
+// // insert in database 
+// $rs = mysqli_query($con, $sql);
+
+// if($rs)
+// {
+// 	echo "Contact Records Inserted";
+// } else {
+//     echo "Error creating database: " . $conn->error;
+//   }
+
+  $username = $_GET["username"];
+  $password = $_GET["password"];
 
   mysqli_connect("localhost", "root", "");
   mysql_select_db("register");
   mysqli_query("INSERT INTO registerTest VALUES('$username', '$password')");
 
-  ?>
-    
-</body>
-</html>
+
+?>
